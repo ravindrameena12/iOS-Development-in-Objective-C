b398a83a3c61fa9745a11461dde0f0e2ebab5737
Vidyashri <vidyashri.mulay@capgemini.com>
2019-11-29 17:25:07 +0530

    Static Table View

---
+++ StaticTableView/StaticTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableView
+//
+//  Created by Vidyashri on 08/09/1941 Saka.
+//  Copyright © 1941 CG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StaticTableView/StaticTableView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  StaticTableView
+//
+//  Created by Vidyashri on 08/09/1941 Saka.
+//  Copyright © 1941 CG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
